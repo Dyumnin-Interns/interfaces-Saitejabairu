ceac23fb35ee4f0be713ed23613093691ee17f45
Saitejabairu <131595664+Saitejabairu@users.noreply.github.com>
2025-05-03 02:39:02 +0530

    Update write_Driver.py

--- tests/Drivers/write_Driver.py
+++ tests/Drivers/write_Driver.py
@@ -1,5 +1,5 @@
 from cocotb_bus.drivers import BusDriver
-
+from cocotb.triggers import Timer, ClockCycles, RisingEdge, Event , ReadOnly
 class write_Driver(BusDriver):
     _signals=["CLK", "RST_N", "write_address", "write_data", "write_en", "write_rdy", "read_address", "read_en", "read_rdy", "read_data"]
     def __init__(self, name, entity):
