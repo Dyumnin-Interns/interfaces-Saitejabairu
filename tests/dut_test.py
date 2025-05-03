ca94385449184a9dbdc5318ddd46e95f83fec92c
Saitejabairu <131595664+Saitejabairu@users.noreply.github.com>
2025-05-03 23:52:42 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,23 +1,12 @@
-import asyncio 
-import os 
-import sys 
 
-
-from pathlib import Path
 import cocotb
-from cocotb import runner
-from cocotb.runner import get_runner
 from cocotb.triggers import Timer, ClockCycles, RisingEdge, Event , ReadOnly
-from cocotb.clock import Clock 
+from cocotb.clock import Clock
 from cocotb.log import logging, SimLog
 from cocotb_bus.drivers import BusDriver
-from cocotb_bus.monitors import BusMonitor
-from cocotb_bus.scoreboard import Scoreboard
 from cocotb_coverage.coverage import CoverPoint, CoverCross, coverage_db
-
 import random as rnd
 import constraint
-
 @CoverPoint("top.a",
             xf = lambda x,y:x,
             bins=[0,1])
